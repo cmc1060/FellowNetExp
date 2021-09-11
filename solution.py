e7657035553c93ecfbb9aefe4b81133432e0451b
cmc1060 <90365726+cmc1060@users.noreply.github.com>
2021-09-11 02:01:56 -0400

    Add files via upload

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 def welcome_assignment_answers(question):
     #The student doesn't have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
+    answer = "0"
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
@@ -14,9 +15,9 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = "1"
+        answer = "5"
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = "2"
+        answer = "4"
     return(answer)
 # Complete all the questions.
 
